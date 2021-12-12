57a229bbffb115cb373f4446f304498661e8575a
stefmau2 <63603675+stefmau2@users.noreply.github.com>
2021-12-12 19:33:02 -0300

    Add files via upload

---
+++ externo.js
@@ -0,0 +1,27 @@
+/*var numero1= prompt('dame un numero');
+var numero2= prompt('dame otro numero');
+
+var resultado =numero1 % numero2;
+alert(resultado);*/
+
+/*var texto = prompt('introduce un texto');
+var texto1= texto.charAt(texto.length-1);
+console.log(texto1);*/
+
+/*var cadena1 = prompt("introduce una palabra");
+var cadena2 = prompt("introduce una palabra");
+var cadena3 = prompt("introduce una palabra");
+
+texto1 = cadena1.length;
+texto2 = cadena2.length;
+texto3 = cadena3.length;
+
+resultado = alert(texto1 + texto2 + texto3);*/
+
+var nombre = prompt("introduce nombre");
+var apellido = prompt("introduce apellido");
+var apellido2 = prompt("introduce apellido");
+
+console.log( nombre + " " +  apellido + " " + apellido2);
+
+
